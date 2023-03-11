ee1e95cdcde17ac0de93bc7da50e7bf88377185b
nikolab20 <nikola.bakic20@hotmail.com>
2023-03-11 19:49:55 +0100

    Initial structure

---
+++ backend/src/main/java/com/bakic/nikola/used_cars/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.bakic.nikola.used_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
